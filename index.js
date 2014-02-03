208c9a3da6b7a5dc6bdd8cda5d6e1c6f07e96508
Jonathan Morley <morley.jonathan@gmail.com>
2014-02-03 01:33:33 -0500

    Add gitignore

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+var mongo = require('mongodb');
+var db = require('monk')('localhost/subway-info');
 var express = require('express');
+var app = new express();
 
-// Constants
-var PORT = 8080;
+var PORT = 3000;
 
-// App
-var app = express();
-app.get('/', function(req,res) {
-  res.send('Hello World\n');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  db.get('subways').find()
+    .success(function(subways){
+      res.json(subways);
+    });
 });
+
+app.get('/:subwayId', function(req, res){
+  db.get('subways').findOne({slug: req.params.subwayId})
+    .success(function(subway){
+      res.json(subway);
+    });
+});
+
+//app.get('/:subwayId/lines');
+//app.get('/:subwayId/lines/:lineId');
+
+//app.get('/:subwayId/stations');
+//app.get('/:subwayId/stations/:stationId');
+
+//app.get('/:subwayId/routes');
+//app.get('/:subwayId/routes/:originId');
+//app.get('/:subwayId/routes/:originId/:destinationId');
 
 app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
